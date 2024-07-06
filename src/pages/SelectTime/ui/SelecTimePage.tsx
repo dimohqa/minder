dd103e435f6d885f9e3becc43e05bb32e95ea5cb
Dmitriy Shestov <dimohqa@skbkontur.ru>
2024-07-06 18:38:07 +0700

    add select time block and submit button

--- src/pages/SelectTime/ui/SelecTimePage.tsx
+++ src/pages/SelectTime/ui/SelecTimePage.tsx
@@ -1,26 +1,73 @@
-import { Calendar, Navigation } from "@/shared/ui/Calendar";
 import { Steps, Title } from "@telegram-apps/telegram-ui";
 import { progressCount } from "../model/const/constants";
-import dayjs from "dayjs";
 import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import { Button } from "@telegram-apps/telegram-ui";
+import { ru } from "date-fns/locale";
+import { addMonths, getYear } from "date-fns";
 
 import styles from "./SelectTimePage.module.css";
 
+// TODO: Add fetch times from API
+const availableTimes = [
+  "13:00",
+  "14:00",
+  "15:00",
+  "19:00",
+  "21:00",
+  "22:00",
+  "13:10",
+  "14:10",
+  "15:10",
+  "19:10",
+  "21:10",
+  "22:10",
+] as const;
+
 export const SelectTimePage = () => {
-  const [monthIndex, setMonthIndex] = useState<number>(dayjs().month());
-  const [year, setYear] = useState<number>(dayjs().year());
+  const [selectedDate, setSelectedDate] = useState<Date | undefined>(new Date());
+  const [selectedTime, setSelectedTime] = useState<string | null>(null);
+
+  const onChangeSelectedDate = (selectedDate: Date | undefined) => {
+    setSelectedDate(selectedDate);
+    setSelectedTime(null);
+  }
+
+  const submitButtonIsVisible = selectedDate && selectedTime;
 
   return (
     <div className={styles.wrapper}>
       <Steps count={3} progress={progressCount} />
-      <Title level="1" className={styles.title}>Время</Title>
-      <Navigation
-        year={year}
-        monthIndex={monthIndex}
-        changeYear={setYear}
-        changeMonthIndex={setMonthIndex}
+      <Title level="1" className={styles.title}>
+        Дата
+      </Title>
+      <DayPicker
+        mode="single"
+        captionLayout="dropdown-buttons"
+        fromMonth={new Date()}
+        toMonth={addMonths(new Date(), 1)}
+        locale={ru}
+        fromYear={getYear(new Date())}
+        toYear={getYear(new Date())}
+        selected={selectedDate}
+        disabled={{ before: new Date(), after: addMonths(new Date(), 1) }}
+        onSelect={onChangeSelectedDate}
       />
-      <Calendar />
+      <div className={styles.availableTimes}>
+        {availableTimes.map((availableTime) => (
+          <Button
+            key={availableTime}
+            mode={selectedTime === availableTime ? "filled" : "gray"}
+            className={styles.timeButton}
+            onClick={() => setSelectedTime(availableTime)}
+          >
+            {availableTime}
+          </Button>
+        ))}
+      </div>
+      <div className={styles.submitButtonWrapper}>
+        {submitButtonIsVisible && (<Button size="l" className={styles.submitButton}>Продолжить</Button>)}
+      </div>
     </div>
   );
 };
